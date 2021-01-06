ac1544aa184a99cf3c5d91c926236247aa0eca6f
yk4r2 <zhestov.ea@phystech.edu>
2021-01-06 20:08:16 +0300

    completed pizza.py

--- pizza.py
+++ pizza.py
@@ -14,7 +14,7 @@ class PizzaBase:
     Has a returnable dict of basic ingredients, their amount and size.
     """
 
-    def __init__(self, size: str = "L"):
+    def __init__(self, size: str = "L") -> None:
         self.size = size
         ingredients = defaultdict(int)
         ingredients.setdefault(0)
@@ -23,20 +23,53 @@ class PizzaBase:
         self._ingredients = ingredients
 
     @property
-    def size(self):
+    def size(self) -> str:
         return self._size
 
     @property
-    def ingredients(self):
+    def ingredients(self) -> defaultdict:
         return self._ingredients
 
     @size.setter
-    def size(self, value: str = "L"):
+    def size(self, value: str = "L") -> None:
         """This weird setter checkup is how it is made in EAFP, lol."""
         try:
             self._size = Size[value].name
         except KeyError:
             print("Size is not suitable.")
 
-    def anime(self):
-        pass
+    def __hash__(self) -> int:
+        return hash(self.ingredients)
+
+    def __eq__(self, other) -> bool:
+        """Better type of checkup."""
+        if isinstance(other, PizzaBase):
+            return self.ingredients == other.ingredients
+        raise TypeError("Plz compare pizza and pizza.")
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__
+
+
+class Margherita(PizzaBase):
+    """Everything is clear here.
+
+    A margherita pizza class.
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super(Margherita, self).__init__(*args, **kwargs)
+        self._ingredients["tomatoes"] = 250
+
+
+class Pepperoni(PizzaBase):
+    """Wow! A Pepperoni pizza class!"""
+    def __init__(self, *args, **kwargs) -> None:
+        super(Pepperoni, self).__init__(*args, **kwargs)
+        self._ingredients["pepperoni"] = 250
+
+
+class Hawaiian(PizzaBase):
+    """No docstring here (actually there is one)."""
+    def __init__(self, *args, **kwargs) -> None:
+        super(Hawaiian, self).__init__(*args, **kwargs)
+        self._ingredients["pineapples"] = 300
