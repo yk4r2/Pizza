4f5b0765bef3076602b2e576b12a8378a3c8cc30
yk4r2 <zhestov.ea@phystech.edu>
2021-01-06 19:41:19 +0300

    some addons to baseClass

--- pizza.py
+++ pizza.py
@@ -1,8 +1,42 @@
-class PizzaBase:
-    """Here be my beautiful docstring."""
+from collections import defaultdict
+from enum import Enum
 
-    def __init__(self, size: str = 'L'):
-        pass
+
+class Size(Enum):
+    """This is done for sizes checkup using the EAFP-paradigm."""
+    L = 1
+    XL = 2
+
+
+class PizzaBase:
+    """A basic class for pizza creating
+
+    Has a returnable dict of basic ingredients, their amount and size.
+    """
+
+    def __init__(self, size: str = "L"):
+        self.size = size
+        ingredients = defaultdict(int)
+        ingredients.setdefault(0)
+        ingredients["tomato sauce"] = 200
+        ingredients["mozzarella"] = 200
+        self._ingredients = ingredients
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def ingredients(self):
+        return self._ingredients
+
+    @size.setter
+    def size(self, value: str = "L"):
+        """This weird setter checkup is how it is made in EAFP, lol."""
+        try:
+            self._size = Size[value].name
+        except KeyError:
+            print("Size is not suitable.")
 
     def anime(self):
         pass
